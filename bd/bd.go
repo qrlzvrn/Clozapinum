eeca47f254161e7f715080e4410b739badf9bd68
qrlzvrn <qrlzvrn@yandex.ru>
2020-02-04 23:37:36 +0300

    Каркас пакета bd

---
+++ bd/bd.go
@@ -0,0 +1,55 @@
+package bd
+
+func CreateUser() error {
+	//
+	//
+	return nil
+}
+
+func CheckUser() error {
+	//
+	//
+	return nil
+}
+
+func ChangeUserState() error {
+	//
+	//
+	return nil
+}
+
+func CreateCategory() error {
+	//
+	//
+	return nil
+}
+
+func ListAllCategory() error {
+	//
+	//
+	return nil
+}
+
+func CreateTask() error {
+	//
+	//
+	return nil
+}
+
+func ChangeTask() error {
+	//
+	//
+	return nil
+}
+
+func DeleteTask() error {
+	//
+	//
+	return nil
+}
+
+func CompleteTask() error {
+	//
+	//
+	return nil
+}
